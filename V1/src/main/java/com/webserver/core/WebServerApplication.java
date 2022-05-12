73b700225a8b5807a1ce31cdf120b4f588e83f3d
jason <jason@gmail.com>
2022-05-12 16:03:45 +0800

    V1

--- V1/src/main/java/com/webserver/core/WebServerApplication.java
+++ V1/src/main/java/com/webserver/core/WebServerApplication.java
@@ -1,6 +1,8 @@
 package com.webserver.core;
 
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * WebServer是模拟Tomcat的一个Web容器
@@ -14,11 +16,28 @@ public class WebServerApplication {
     private ServerSocket serverSocket;
 
     public WebServerApplication(){
-
+        try {
+            System.out.println("正在启动服务端...");
+            serverSocket = new ServerSocket(8088);
+            System.out.println("服务端启动完毕!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
     public void start(){
+        try {
+            System.out.println("等待客户端链接...");
+            Socket socket = serverSocket.accept();
+            System.out.println("一个客户端链接了!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
+
+
+
     public static void main(String[] args) {
         WebServerApplication application = new WebServerApplication();
         application.start();
