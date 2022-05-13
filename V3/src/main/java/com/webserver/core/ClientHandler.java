2f02c28ced60bdcf6997ada21240a7c22b06ed5d
Xiloer <83123095@qq.com>
2022-05-13 10:10:57 +0800

    V3实现读取一行字符串操作

--- V3/src/main/java/com/webserver/core/ClientHandler.java
+++ V3/src/main/java/com/webserver/core/ClientHandler.java
@@ -18,10 +18,29 @@ public class ClientHandler implements Runnable{
     public void run() {
         try {
             InputStream in = socket.getInputStream();
-            int d;
+            /*
+                测试读取一行字符串
+                思路:
+                连续读取若干字符，当连续读取到了回车符和换行符就停止。并将之前
+                连续读取的字符组成一个字符串。
+             */
+
+            //使用StringBuilder用于拼接每一个读取到的字符，并最终组成一个字符串使用
+            StringBuilder builder = new StringBuilder();
+            int d;//记录每次读取到的字节
+            char cur = 'a';//表示本次读取到的字符
+            char pre = 'a';//表示上次读取到的字符
             while((d = in.read()) != -1){
-                System.out.print((char)d);
+                cur = (char)d;//将本次读取到的字节转换为char记录
+                if(pre==13 && cur==10){//判断上次读取的是否为回车符并且本次读取到的是否为换行符
+                    //如果连续读取到了回车+换行符，则停止本行字符串的读取工作
+                    break;
+                }
+                builder.append(cur);//将本次读取的字符拼接到字符串中
+                pre = cur;//在读取下一个字符前，将本次读取的字符记作上次读取的字符
             }
+            String line = builder.toString().trim();
+            System.out.println("请求行内容:"+line);
 
 
 
