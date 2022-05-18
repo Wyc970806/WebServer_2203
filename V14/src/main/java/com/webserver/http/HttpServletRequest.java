9ad2bbdb0a0ac4332307918fde82849710a23c9b
Xiloer <83123095@qq.com>
2022-05-18 12:01:09 +0800

    V14 用户注册

--- V14/src/main/java/com/webserver/http/HttpServletRequest.java
+++ V14/src/main/java/com/webserver/http/HttpServletRequest.java
@@ -20,6 +20,11 @@ public class HttpServletRequest {
     private String uri;//抽象路径
     private String protocol;//协议版本
 
+    private String requestURI;//保存uri中的请求部分(?左侧内容)
+    private String queryString;//保存uri中的参数部分(?右侧内容)
+    private Map<String,String> parameters = new HashMap<>();//保存每一组参数
+
+
     //消息头相关信息
     //这个Map存所有消息头，key为消息头的名字 value为消息头的值
     private Map<String,String> headers = new HashMap<>();
@@ -51,10 +56,37 @@ public class HttpServletRequest {
         uri = data[1];
         protocol = data[2];
 
+        parseUri();//进一步解析uri
+
         System.out.println("method:"+method);
         System.out.println("uri:"+uri);
         System.out.println("protocol:"+protocol);
     }
+
+    /**
+     *  进一步解析uri
+     */
+    private void parseUri(){
+        /*
+            uri是有两种情况的，1:不含有参数的 2:含有参数的
+            例如:
+            不含有参数的:/myweb/reg.html
+            含有参数的:/myweb/reg?username=fanchuanqi&password=123456&nickname=chuanqi&age=22
+
+            处理方式:
+            1:若不含有参数，则直接将uri的值赋值给requestURI
+            2:若含有参数
+              2.1:先将uri按照"?"拆分为请求部分和参数部分
+                  将请求部分赋值给requestURI
+                  将参数部分赋值给queryString
+              2.2:再将参数部分按照"&"拆分出每一组参数
+                  每组参数再按照"="拆分为参数名和参数值
+                  并将参数名作为key，参数值作为value保存到parameters这个Map中
+
+              允许页面输入框空着，这种情况该参数的值为空字符串存入parameters即可
+         */
+    }
+
     /**
      * 解析消息头
      */
