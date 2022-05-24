48305490e8ecd9b821ae706c7d11567f0876cceb
Xiloer <83123095@qq.com>
2022-05-24 17:02:01 +0800

    V24 利用反射机制重构DispatcherServlet

--- V24/src/main/java/com/webserver/controller/ArticleController.java
+++ V24/src/main/java/com/webserver/controller/ArticleController.java
@@ -3,13 +3,13 @@ package com.webserver.controller;
 import com.webserver.annotation.Controller;
 import com.webserver.annotation.RequestMapping;
 import com.webserver.entity.Article;
+import com.webserver.entity.User;
 import com.webserver.http.HttpServletRequest;
 import com.webserver.http.HttpServletResponse;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 处理与文章相关的业务
@@ -57,4 +57,60 @@ public class ArticleController {
 
 
     }
+
+    @RequestMapping("/myweb/showAllArticle")
+    public void showAllArticle(HttpServletRequest request,HttpServletResponse response){
+        //1将articles目录下的所有obj文件进行反序列化，得到所有的文章信息
+        List<Article> articleList = new ArrayList<>();
+        File[] articles = articleDir.listFiles(f->f.getName().endsWith(".obj"));
+        for(File articleFile : articles){
+            try (
+                    ObjectInputStream ois = new ObjectInputStream(
+                            new FileInputStream(
+                                    articleFile
+                            )
+                    )
+            ){
+                Article article = (Article)ois.readObject();
+                articleList.add(article);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        /*
+            2生成一个HTML页面(动态页面)并包含所有文章数据
+         */
+        //添加响应头，用于将来告知浏览器该动态数据是一个html页面
+        response.setContentType("text/html");
+
+        PrintWriter pw = response.getWriter();
+        pw.println("<!DOCTYPE html>");
+        pw.println("<html lang=\"en\">");
+        pw.println("<head>");
+        pw.println("<meta charset=\"UTF-8\">");
+        pw.println("<title>文章列表</title>");
+        pw.println("</head>");
+        pw.println("<body>");
+        pw.println("<center>");
+        pw.println("<h1>文章列表</h1>");
+        pw.println("<table border=\"1\">");
+        pw.println("<tr>");
+        pw.println("<td>标题</td>");
+        pw.println("<td>作者</td>");
+        pw.println("</tr>");
+        for(Article article : articleList) {
+            pw.println("<tr>");
+            pw.println("<td>"+ article.getTitle()+"</td>");
+            pw.println("<td>"+ article.getAuthor()+"</td>");
+            pw.println("</tr>");
+        }
+        pw.println("</table>");
+        pw.println("</center>");
+        pw.println("</body>");
+        pw.println("</html>");
+    }
+
+
 }
