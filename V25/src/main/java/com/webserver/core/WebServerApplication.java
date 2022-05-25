2b1ce7a6c48e6d7316ef737167275ca01816e8e2
Xiloer <83123095@qq.com>
2022-05-25 11:57:08 +0800

    V25 重构代码

--- V25/src/main/java/com/webserver/core/WebServerApplication.java
+++ V25/src/main/java/com/webserver/core/WebServerApplication.java
@@ -3,6 +3,8 @@ package com.webserver.core;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * WebServer是模拟Tomcat的一个Web容器
@@ -14,11 +16,13 @@ import java.net.Socket;
  */
 public class WebServerApplication {
     private ServerSocket serverSocket;
+    private ExecutorService threadPool;
 
     public WebServerApplication(){
         try {
             System.out.println("正在启动服务端...");
             serverSocket = new ServerSocket(8088);
+            threadPool = Executors.newFixedThreadPool(50);
             System.out.println("服务端启动完毕!");
         } catch (IOException e) {
             e.printStackTrace();
@@ -33,8 +37,7 @@ public class WebServerApplication {
                 System.out.println("一个客户端链接了!");
                 //启动一个线程处理该客户端交互
                 ClientHandler handler = new ClientHandler(socket);
-                Thread t = new Thread(handler);
-                t.start();
+                threadPool.execute(handler);
             }
 
         } catch (IOException e) {
